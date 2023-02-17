ea417ec17cdeb8d088c88080374162a1e9b9c9e6
Prakruti Ukani <prakruti.ukani@tatvamail.com>
2023-02-17 15:27:31 +0530

    added validation

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,38 +1,64 @@
 import axios from 'axios'
+import { useFormik } from 'formik'
 import React, { useState } from 'react'
+import { formSchema } from '../schemas'
 
 const baseUrl = 'jzipbscz0.g.tau.link'
-const postUrl = async(baseUrl, url) => {
-	const res = await axios.post(`https://${baseUrl}/shorten`, {
-		url,
-		base_url: baseUrl
-	})
-	return res.data
+const postUrl = async (baseUrl, url) => {
+    const res = await axios.post(`https://${baseUrl}/shorten`, {
+        url,
+        base_url: baseUrl
+    })
+    return res.data
 }
 
 const Dashboard = () => {
 
-    const [url, setUrl] = useState('')
     const [shortenUrl, setShortenUrl] = useState('')
 
-    const handleSubmit = async () => {
-        const data = await postUrl(baseUrl, url);
-		if(data) setShortenUrl(data.url)
-		setUrl('')
+    const initialValues = {
+        url: ""
     }
 
-    const handleChange = (e) => {
-        setUrl(e.target.value);
-    }
+    const {
+        values,
+        errors,
+        handleChange,
+        handleBlur,
+        resetForm,
+        handleSubmit
+    } = useFormik({
+        initialValues: initialValues,
+        validationSchema: formSchema,
+        onSubmit: async (values) => {
+            const data = await postUrl(baseUrl, values.url);
+            if (data) setShortenUrl(data.url)
+            resetForm();
+        }
+    });
 
     return (
-        <div>
+        <div className='container'>
             <h2>Paste the URL to be shortened!</h2>
-            <input type="text" placeholder={"Enter URL here"} onChange={handleChange} value={url} />
-            <button onClick={handleSubmit}>Make it short!</button>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        type="text"
+                        placeholder={"Enter URL here"}
+                        name="url"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.url} 
+                        style={{width: '300px', height: '30px'}}
+                        /><br />
+                    {errors && errors.url && (<span style={{ color: "red" }}>{errors.url}</span>)}
+                </div>
+                <button type="submit">Make it short!</button>
+            </form>
             {
                 shortenUrl && <h3>Shorten URL: <a href={shortenUrl}>{shortenUrl}</a></h3>
             }
+
         </div>
     )
 }
---
+++ src/schemas/index.js
@@ -0,0 +1,8 @@
+import * as Yup from 'yup'
+
+export const formSchema = Yup.object({
+    url: Yup.string().matches(
+        /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+        'Please Enter Correct URL!'
+    ).required()
+})
